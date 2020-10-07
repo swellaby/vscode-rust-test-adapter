9a711507180634ea1b753de039ff45edd3b3b7d2
Caleb Cartwright <caleb.cartwright@outlook.com>
2020-10-06 20:24:21 -0500

    chore: tweak eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,17 @@ module.exports = {
             rules: {
                 'no-unused-vars': [ 'off' ]
             }
+        },
+        // https://github.com/swellaby/vscode-rust-test-adapter/pull/89#issuecomment-704637554
+        {
+            files: [
+                'src/cargo.js',
+                'src/test-loader.js',
+                'test/data/tree-nodes.js'
+            ],
+            rules: {
+                'max-len': [ 'off' ]
+            }
         }
     ]
 };
